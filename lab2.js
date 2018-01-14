e57ea0209dd2b3ad3b652664159ccdbfd15d0dae
justfry <dondimon777@gmail.com>
2018-01-14 22:20:31 +0300

    First commit

---
+++ lab2.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+const path = require('path')
+var Converter = require("csvtojson").Converter;
+
+const pathoffile = path.join(__dirname, 'data-to-convert', 'customer-data.csv') 
+var converter = new Converter({})
+
+const convertCsvToJson = (pathToFile = pathoffile) => {
+
+    console.log('Converting has start')
+
+    converter.fromFile(pathToFile,function(err,result){
+        if(err){
+            console.log("An Error Has Occured")
+            console.log(err)
+        } 
+        var json = JSON.stringify(result)
+        fs.writeFileSync(path.join(__dirname, "output", path.basename(pathToFile, '.csv') + '.json'), json)
+    });
+}
+convertCsvToJson(process.argv[2])
+
